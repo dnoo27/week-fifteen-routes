c560c25fb0a598d06f9a78b246868bb3315bd01d
David <43051959+dnoo27@users.noreply.github.com>
2018-12-13 18:28:33 -0600

    set up 3 intial routes
    tested them manually by editing urls localhost:4200/foo etc.
    
    const routes: Routes = [
      { path: '', component: FooComponent }
      ,{ path: 'foo', component: FooComponent }
      ,{ path: 'bar', component: BarComponent }
      ,{ path: 'cat', component: CatComponent }
    
    ];

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FooComponent } from './foo/foo.component';
+import { BarComponent } from './bar/bar.component';
+import { CatComponent } from './cat/cat.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: FooComponent }
+  ,{ path: 'foo', component: FooComponent }
+  ,{ path: 'bar', component: BarComponent }
+  ,{ path: 'cat', component: CatComponent }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
