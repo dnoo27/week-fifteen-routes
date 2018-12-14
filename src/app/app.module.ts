76de55ffe2a7d0fa0fe0c8e5ebfb8b77fd95bda3
David <43051959+dnoo27@users.noreply.github.com>
2018-12-13 18:22:21 -0600

    made components
    ng generated foo, bar, and cat
    
    'ng generate component foo'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooComponent } from './foo/foo.component';
+import { BarComponent } from './bar/bar.component';
+import { CatComponent } from './cat/cat.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooComponent,
+    BarComponent,
+    CatComponent
   ],
   imports: [
     BrowserModule,
